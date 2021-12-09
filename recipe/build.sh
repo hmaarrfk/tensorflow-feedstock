fc1ac0ca33276a43b912f65bdd1d9f29b74fba8b
Mark Harfouche <mark.harfouche@gmail.com>
2021-12-08 22:51:33 -0500

    Simplify dependency management

--- recipe/build.sh
+++ recipe/build.sh
@@ -183,3 +183,9 @@ if [[ "${target_platform}" == linux-* ]]; then
 fi
 
 bazel clean
+
+# install the whl using pip
+pip install --no-deps $SRC_DIR/tensorflow_pkg/*.whl
+
+# The tensorboard package has the proper entrypoint
+rm -f ${PREFIX}/bin/tensorboard
