ffd61b9532efc2ee58555ff1287f3cb23bf3007d
Mark Harfouche <mark.harfouche@gmail.com>
2023-03-21 08:52:45 -0400

    Pin thing

--- recipe/cp_libtensorflow.sh
+++ recipe/cp_libtensorflow.sh
@@ -1,4 +1,5 @@
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/lib_package/README.md
+exit 0
 tar -C ${PREFIX} -xzf $SRC_DIR/libtensorflow.tar.gz
 
 # Make writable so patchelf can do its magic
--- recipe/cp_libtensorflow_cc.sh
+++ recipe/cp_libtensorflow_cc.sh
@@ -1,1 +1,2 @@
+exit 0
 tar -C ${PREFIX} -xf $SRC_DIR/libtensorflow_cc_output.tar
